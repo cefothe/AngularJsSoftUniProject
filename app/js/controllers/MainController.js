5cd9a2315448b17e3c656161861a94e2aaeaadb3
cefothe <cefothe@gmail.com>
2015-01-02 18:56:25 +0200

    Add category and town in filters

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -2,5 +2,13 @@ softUni.controller('SoftUniController',function ($scope, mainData){
 	
 	mainData.getAllAds(function(resp){
 		$scope.data=resp;
+	});
+
+	mainData.getAllTown(function(resp){
+		$scope.towns=resp;
+	});
+
+	mainData.getAllCategory(function(resp){
+		$scope.categorys=resp;
 	})
 });
--- app/js/services/mainPage.js
+++ app/js/services/mainPage.js
@@ -10,6 +10,26 @@ softUni.factory('mainData',function ($http, $log){
 			}).error(function(data,status,headers,config){
 				$log.warn(data);
 			});
+		},
+		getAllTown:function(success){
+			$http({
+				method:'GET',
+				url:'http://softuni-ads.azurewebsites.net/api/towns'
+			}).success(function(data,status,headers,config){
+				success(data);
+			}).error(function(data,status,headers,config){
+				$log.warn(data);
+			});
+		},
+		getAllCategory:function(success){
+			$http({
+				method:'GET',
+				url:'http://softuni-ads.azurewebsites.net/api/categories'
+			}).success(function(data,status,headers,config){
+				success(data);
+			}).error(function(data,status,headers,config){
+				$log.warn(data);
+			});
 		}
 
 	}
