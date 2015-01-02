d0adf4aae4f000258ce4344e5f6cce2660d04623
cefothe <cefothe@gmail.com>
2015-01-02 18:05:32 +0200

    Get information from server and visit one page

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,3 +1,6 @@
-softUni.controller('SoftUniController',function ($scope){
-	$scope.name="Name";
+softUni.controller('SoftUniController',function ($scope, mainData){
+	
+	mainData.getAllAds(function(resp){
+		$scope.data=resp;
+	})
 });
